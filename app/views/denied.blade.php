637c26df7e4b18a000efbddb2f174b06f2f6d58d
Tammy Shipps <mizziness@gmail.com>
2015-11-29 18:53:57 -0500

    New denied page

--- app/views/denied.blade.php
+++ app/views/denied.blade.php
@@ -1,14 +1,32 @@
 <!doctype html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Access Denied</title>
-</head>
-
-<body>
-
-	<p>Sorry, you cannot access this resource.</p>
-	
-	
-</body>
+  <head>
+    <meta charset="utf-8">
+    <title>Access Denied | Media MachineSL</title>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="http://v2.mediamachinesl.com/css/gridiculous.css" />
+    <link rel="stylesheet" type="text/css" href="http://v2.mediamachinesl.com/css/styles.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <style type="text/css">
+      html, body { height: 100%; }
+      body { background: url("http://mediamachinesl.com/images/background.jpg") no-repeat bottom left; background-attachment: fixed; }
+      #container { margin: 0 auto; height: 100%; }
+      #container .row { margin: 10px 0; }
+      h2, p { text-align: center; }
+    </style>
+  </head>
+  
+  <body>
+    <div id="container" class="grid">
+      <div class="row">
+         <img src="http://mediamachinesl.com/images/new-MediaMachine-Logo.png" />
+      </div>
+      <div class="row">
+        <div class="c12">
+          <h2>Access Denied</h2>
+          <p>We're sorry, but you cannot access the Media MachineSL system at this time.  You can only connect to our service from approved MMSL devices from within SecondLife.  If you believe you are getting this message in error, contact us in-world for customer support.  Thank you!</p>
+        </div>
+      </div>
+    </div>
+   </body>
 </html>
